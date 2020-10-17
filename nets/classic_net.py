b0c1222262ba13388aac825cdff4cf383cd1cde4
tengyum <tengyu.ma@vanderbilt.edu>
2020-10-16 20:40:10 -0500

    add inception

--- nets/classic_net.py
+++ nets/classic_net.py
@@ -5,12 +5,11 @@ import torchvision.models as models
 
 def get_classicnet(pretrained, net_name):
     if net_name == 'resnet18':
-        net = models.resnet18(pretrained=pretrained)
-        net.fc = nn.Linear(512, 12)
-        return net
+        net = models.resnet18(pretrained=pretrained, num_classes=12)
     elif net_name == 'resnet34':
-        net = models.resnet18(pretrained=pretrained)
-        net.fc = nn.Linear(512, 12)
-        return net
+        net = models.resnet18(pretrained=pretrained, num_classes=12)
+    elif net_name == 'inceptionv3':
+        net = models.inception_v3(pretrained=pretrained, num_classes=12)
     else:
         raise ValueError(f'Invalid net_name {net_name}')
+    return net
