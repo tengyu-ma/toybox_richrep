27c7716938a63f097faf957e8d209981e49432be
tengyum <tengyu.ma@vanderbilt.edu>
2020-10-17 12:17:48 -0500

    update args

--- colab_run.py
+++ colab_run.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     parser.add_argument('-trs', type=str, help='The transformations for the Toybox data')
     parser.add_argument('-nview', type=str, help='How many views to be used for the Toybox data')
     parser.add_argument('-net_type', type=str, help='The net_type for training, classic or rich')
-    parser.add_argument('-net_name', type=str, help='The backbone net for training')
+    parser.add_argument('-net_names', type=str, help='The backbone net for training')
 
     args = parser.parse_args()
 
@@ -17,12 +17,13 @@ if __name__ == '__main__':
     trs = args.trs.split(' ')
     nview = int(args.nview)
     net_type = args.net_type
-    net_name = args.net_name
+    net_names = args.net_names.split(' ')
 
     for r in ratios:
-        if net_name == 'classic':
-            classic_exp.exp_main(ratios=[r], trs=trs, nview=nview, net_name=net_name)
-        elif net_name == 'rich':
-            classic_exp.exp_main(ratios=[r], trs=trs, nview=nview)
-        else:
-            raise ValueError(f'Invalid net type {net_type}')
+        for net_name in net_names:
+            if net_name == 'classic':
+                classic_exp.exp_main(ratios=[r], trs=trs, nview=nview, net_name=net_name)
+            elif net_name == 'rich':
+                rich_exp.exp_main(ratios=[r], trs=trs, nview=nview)
+            else:
+                raise ValueError(f'Invalid net type {net_type}')
