1ebf70d69f158f2adc37e95bdcbaeb8ff968c1ca
tengyum <tengyu.ma@vanderbilt.edu>
2020-10-18 00:49:59 -0500

    pretrain name prefix

--- exps/classic_exp.py
+++ exps/classic_exp.py
@@ -20,7 +20,7 @@ def exp_main(ratios, trs, nview, net_name, pretrained, batch_size, epochs, prelo
         num_workers=0,
         epochs=epochs,
         preload=preload,
-        pretrain='pretrain'
+        pretrain='pretrain' if pretrained else 'no_pretrain'
     )
     tb_trainer = ToyboxTrainer(
         tr=trs,
